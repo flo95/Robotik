1df9eb6c909d0d296f442450478af40e2e332932
oliver <oliver@wlabs.de>
2017-04-21 10:41:06 +0200

    uebung3 struktur

---
+++ src/uebung3/aufgabe1/KroetenwanderungListener.java
@@ -0,0 +1,9 @@
+package uebung3.aufgabe1;
+
+public class KroetenwanderungListener {
+
+	public static void main(String[] args) {
+		new U3A1().run();
+	}
+
+}
---
+++ src/uebung3/aufgabe1/LeftTouchSensorListener.java
@@ -0,0 +1,14 @@
+package uebung3.aufgabe1;
+
+import lejos.nxt.SensorPort;
+import lejos.nxt.SensorPortListener;
+
+public class LeftTouchSensorListener implements SensorPortListener {
+
+	@Override
+	public void stateChanged(SensorPort aSource, int aOldValue, int aNewValue) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
