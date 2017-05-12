12d0ddd03ba4b45ec63e9231ab56471984bc22e9
olikos <oliver@wlabs.de>
2017-05-12 08:45:26 +0200

    lejos.pc aus TicTacToe entfernt

--- src/uebung4/b/TicTacToe.java
+++ src/uebung4/b/TicTacToe.java
@@ -6,10 +6,10 @@ import lejos.nxt.Button;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
-import lejos.pc.comm.NXTComm;
-import lejos.pc.comm.NXTCommFactory;
-import lejos.pc.comm.NXTConnector;
-import lejos.pc.comm.NXTInfo;
+//import lejos.pc.comm.NXTComm;
+//import lejos.pc.comm.NXTCommFactory;
+//import lejos.pc.comm.NXTConnector;
+//import lejos.pc.comm.NXTInfo;
 
 public class TicTacToe {
 
@@ -17,7 +17,7 @@ public class TicTacToe {
 
 	private static BTConnection btConnector = null;
 
-	private static NXTConnector nxtConnector;
+	// private static NXTConnector nxtConnector;
 
 	public static void main(String[] args) {
 		// example Vorlesung
@@ -86,98 +86,100 @@ public class TicTacToe {
 
 	private static void handleMaster() {
 		// search for connections and display them
-		NXTInfo[] nxts;
-		nxtConnector = new NXTConnector();
-		System.out.println("Suche BT NXTs ...");
-		nxts = nxtConnector.search("", null, NXTCommFactory.BLUETOOTH);
-		int i = 1;
-		for (NXTInfo nxtInfo : nxts) {
-			System.out.println(i + ": " + nxtInfo.name + " " + nxtInfo.deviceAddress);
-			i++;
-		}
-		boolean deviceToConnectSelected = false;
-		int selected = 0;
-		while (!deviceToConnectSelected) {
-			if (Button.RIGHT.isDown()) {
-				selected++;
-				System.out.println(selected);
-			} else if (Button.LEFT.isDown()) {
-				selected--;
-				System.out.println(selected);
-			} else if (Button.ENTER.isDown()) {
-				// try to connect to device with index selected
-				if (!nxtConnector.connectTo(nxts[selected], NXTComm.PACKET)) {
-					System.out.println("Konnte nicht verbinden ...");
-					System.exit(0);
-				}
-				System.out.println("Verbindung erfolgreich, das Spiel kann beginnen!");
-				playGameMaster();
-
-			}
-		}
+		// NXTInfo[] nxts;
+		// nxtConnector = new NXTConnector();
+		// System.out.println("Suche BT NXTs ...");
+		// nxts = nxtConnector.search("", null, NXTCommFactory.BLUETOOTH);
+		// int i = 1;
+		// for (NXTInfo nxtInfo : nxts) {
+		// System.out.println(i + ": " + nxtInfo.name + " " +
+		// nxtInfo.deviceAddress);
+		// i++;
+		// }
+		// boolean deviceToConnectSelected = false;
+		// int selected = 0;
+		// while (!deviceToConnectSelected) {
+		// if (Button.RIGHT.isDown()) {
+		// selected++;
+		// System.out.println(selected);
+		// } else if (Button.LEFT.isDown()) {
+		// selected--;
+		// System.out.println(selected);
+		// } else if (Button.ENTER.isDown()) {
+		// // try to connect to device with index selected
+		// if (!nxtConnector.connectTo(nxts[selected], NXTComm.PACKET)) {
+		// System.out.println("Konnte nicht verbinden ...");
+		// System.exit(0);
+		// }
+		// System.out.println("Verbindung erfolgreich, das Spiel kann
+		// beginnen!");
+		// playGameMaster();
+		//
+		// }
+		// }
 	}
 
 	private static void playGameMaster() {
 		System.out.println("Wählen Sie eine Position:\n");
 		printGame();
 		// Get NXTComm Object for I / O
-		NXTComm comm = nxtConnector.getNXTComm();
-		while (true) {
-			// select position
-			write(comm);
-		}
+		// NXTComm comm = nxtConnector.getNXTComm();
+		// while (true) {
+		// // select position
+		// write(comm);
+		// }
 	}
 
-	private static void write(NXTComm comm) {
-		int position = 0;
-		while (!Button.ENTER.isDown()) {
-			if (Button.RIGHT.isDown()) {
-				position++;
-				System.out.println(position);
-			} else if (Button.LEFT.isDown()) {
-				position--;
-				System.out.println(position);
-			}
-		}
-		String toSend = String.valueOf(position);
-		try {
-			// check if value is possible
-			if (field[position--].equals(" ")) {
-				field[position--] = "X";
-				printGame();
-			} else {
-				System.out.println("Fehler: try again");
-				write(comm);
-			}
-			comm.write(toSend.getBytes());
-			read(comm);
-			// wait for answer;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-	private static void read(NXTComm comm) {
-		try {
-			byte[] b = comm.read();
-			String readIn = String.valueOf(b);
-			Integer poisition = Integer.valueOf(readIn);
-			field[poisition--] = "O";
-			printGame();
-			write(comm);
-		} catch (Exception e) {
-		}
-
-	}
+	// private static void write(NXTComm comm) {
+	// int position = 0;
+	// while (!Button.ENTER.isDown()) {
+	// if (Button.RIGHT.isDown()) {
+	// position++;
+	// System.out.println(position);
+	// } else if (Button.LEFT.isDown()) {
+	// position--;
+	// System.out.println(position);
+	// }
+	// }
+	// String toSend = String.valueOf(position);
+	// try {
+	// // check if value is possible
+	// if (field[position--].equals(" ")) {
+	// field[position--] = "X";
+	// printGame();
+	// } else {
+	// System.out.println("Fehler: try again");
+	// write(comm);
+	// }
+	// comm.write(toSend.getBytes());
+	// read(comm);
+	// // wait for answer;
+	// } catch (Exception e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	// }
+	//
+	// private static void read(NXTComm comm) {
+	// try {
+	// byte[] b = comm.read();
+	// String readIn = String.valueOf(b);
+	// Integer poisition = Integer.valueOf(readIn);
+	// field[poisition--] = "O";
+	// printGame();
+	// write(comm);
+	// } catch (Exception e) {
+	// }
+	//
+	// }
 
 	private static void printGame() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 9; i++) {
 			if (i % 3 == 0) {
 				sb.append(field[i] + "\n");
-				sb.append("-----");
+				sb.append("----------");
 			} else {
 				sb.append(field[i] + " | ");
 			}
